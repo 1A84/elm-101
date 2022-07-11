2e46a0376b99b4f7f2be530d31c09078e8bed87e
Luca <nillability@protonmail.com>
2022-07-11 16:15:01 +0900

    fix warning

--- views/src/Main.elm
+++ views/src/Main.elm
@@ -6,15 +6,15 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-import Url
 import Url exposing (..)
 import Url.Parser exposing ( oneOf, map )
 
-import Json.Decode as D
-
 import Shared exposing (..)
+import Route exposing (Route (..), toString, fromUrl)
+import Bar exposing (..)
 -- import Route exposing ( Route, fromUrl )
 import Pages.Select as Select exposing ( view )
+import Pages.Slide as Slide exposing ( view )
 
 
 
@@ -41,6 +41,7 @@ type alias Model =
     { route : Route
     , shared : Shared.Model
     , key : Nav.Key
+    , slide : Shared.Slide
     }
 
 init : () -> Url -> Nav.Key -> (Model, Cmd Msg)
@@ -53,6 +54,7 @@ init flags url key =
     ({ route = fromUrl url
     , shared = shared
     , key = key
+    , slide = Shared.slide
     }
     , Cmd.none
     )
@@ -72,7 +74,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UrlChanged url ->
-            if url.path /= toString model.route then
+            if url.path /= Route.toString model.route then
                 ( { model | route = fromUrl url }
                 , Cmd.none
                 )
@@ -98,6 +100,15 @@ update msg model =
                     ( model, Nav.pushUrl model.key (Url.toString url))
                 Browser.External href ->
                     ( model, Nav.load href)
+        
+        -- SlideMsg slide ->
+        --     let
+        --         ( slide_, slideCmd ) =
+        --             Slide.update slide model.shared
+        --     in
+        --     ({ model | shared = slide_ }
+        --     , Cmd.none
+        --     )
 
       
 
@@ -111,7 +122,11 @@ view model  =
             -- div [ class "container" ]
             -- [
             { title = "Elm"
-            , body = [ Select.view SharedMsg model.shared ]
+            , body = [ container [
+                Bar.view model.route
+                , Select.view SharedMsg model.shared
+                ]
+            ]
             }
             -- ]
             -- div [ class "container" ]
@@ -134,40 +149,41 @@ view model  =
         Slide ->
             -- Select.view SharedMsg model.shared
             { title = "Elm"
-            , body = [ Select.view SharedMsg model.shared ]
+            , body = [ container [ Bar.view model.route
+            , Slide.view SharedMsg model.shared model.slide] ]
             }
             -- div [ class "container" ]
             --     [ Html.map <| Select.view model.shared ]
     
-type Route
-    = Select
-    | Slide
+-- type Route
+--     = Select
+--     | Slide
 
 
-fromUrl : Url -> Route
-fromUrl url =
-    case url.path of
-        "/1.0" ->
-            Select
+-- fromUrl : Url -> Route
+-- fromUrl url =
+--     case url.path of
+--         "/1.0" ->
+--             Select
 
-        "/2.0" ->
-            Slide
+--         "/2.0" ->
+--             Slide
 
-        _ ->
-            Select
+--         _ ->
+--             Select
 
-toString : Route -> String
-toString r =
-    case r of
-        Select ->
-            "/1.0"
+-- toString : Route -> String
+-- toString r =
+--     case r of
+--         Select ->
+--             "/1.0"
 
-        Slide ->
-            "/2.0"
+--         Slide ->
+--             "/2.0"
 
-valueDecoder : D.Decoder String
-valueDecoder =
-    D.field "currentTarget" (D.field "value" D.string)
+-- valueDecoder : D.Decoder String
+-- valueDecoder =
+--     D.field "currentTarget" (D.field "value" D.string)
 -- coverImage : Model -> Attribute msg
 -- coverImage model =
 --   let
