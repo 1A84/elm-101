ada0f82638223ac3597fb9dcbf4e351abee99da9
Luca <nillability@protonmail.com>
2022-07-11 16:21:47 +0900

    remove comment-out code

--- views/src/Pages/Slide.elm
+++ views/src/Pages/Slide.elm
@@ -4,92 +4,12 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import List exposing ( tail )
-import Maybe exposing ( map )
 import Maybe.Extra as MaybeNg exposing (..)
 import List.Extra as List exposing (..)
 
 import Image exposing (..)
 import Shared exposing ( Model, Msg, Slide )
 import Shared exposing (viewImage)
-import Json.Decode exposing (maybe)
--- import String exposing (left)
-
-
--- INIT
-
--- slide : List Img
--- slide = [ Lain, AudreyTang, ChinoChan ]
-
--- type alias Model = Int
-
-
-
--- UPDATE
--- type alias Slide = List Img
--- init : Slide
--- init =
---     [ Lain, AudreyTang, ChinoChan ]
-
-
--- type Msg
---     = Left Slide
---     | Right Slide
-
-
--- update : Msg -> Model -> ( Model, Cmd msg)
--- update msg model =
---     case msg of
---         Left slide ->
---             -- if index_ == 0 then
---             --     ( model, Cmd.none )
-
---             -- else
---                 -- ( { model | img = v_ }
---                 -- , Cmd.none
---                 -- )
---             elemIndex model.img slide
---                 |> MaybeNg.filter (\n -> n /= 0)
---                 |> Maybe.map (\n -> n - 1)
---                 |> Maybe.map (\n -> getAt n slide)
---                 |> MaybeNg.join
---                 |> MaybeNg.unwrap ( model, Cmd.none ) (\n -> ( Shared.updateImg, Cmd.none))
---             -- ( model - 1, Cmd.none )
-
---         Right slide ->
---             -- ( model + 1, Cmd.none )
---             let 
---                 l = last slide
---             in
---             case l of
---                 Just l_ ->
---                     let index = elemIndex l_ slide
---                     in
---                     case index of
---                         Just index_ ->
---                             case (getAt (index_ + 1) slide) of
---                                 Just v ->
---                                     ({ model | img = v}, Cmd.none)
-                                
---                                 _ -> (model, Cmd.none)
-                        
---                         _ -> (model, Cmd.none)
-
---                 _ -> (model, Cmd.none)
-            -- last slide
-            --     |> MaybeNg.filter (\n -> n /= model.img)
-            --     |> Maybe.map (\n -> elemIndex n slide)
-            --     |> MaybeNg.join
-            --     |> Maybe.map (\n -> n + 1)
-            --     |> Maybe.map (\n -> getAt n slide)
-            --     |> MaybeNg.join
-            --     |> MaybeNg.unwrap ( model, Cmd.none ) (\n -> ( { model | img = n }, Cmd.none))
-            -- if index_ == 0 then
-                
-
--- left : Model -> ( Model, Cmd msg)
--- left model = 
---     case model.img of
---         "lain" -> model.length
 
 
 -- VIEW
